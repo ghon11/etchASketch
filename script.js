023e25bebf550b60fc414f78eb92f718d72a2153
Garry Yang <yarrygang@gmail.com>
2022-09-01 20:49:09 +0100

    fixed grid size regardless of no. rows/columns. centered grid. adding max value for prompt.

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
-const gridSize = document.querySelector('#gridSize');
+const gridWidth = 960;
 
-gridSize.addEventListener('click', () => {
-    let gridValue = prompt('Specify a grid size:');
-    generateGrid(gridValue);
+const promptGridSize = document.querySelector('#gridSize');
+
+promptGridSize.addEventListener('click', () => {
+    let gridSize = prompt('Specify a grid size:');
+    if (gridSize < 100) {
+        console.log("Value too large, enter a smaller value (less than 100).");
+        break;
+    }
+    generateGrid(gridSize);
 })
 
-function generateGrid(gridValue) {
-    for (let i = 0; i < gridValue; i++) {
-        for (let j = 0; j < gridValue; j++) {
+function generateGrid(gridSize) {
+    grid.innerHTML = '';
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
             const cell = document.createElement('div');
             cell.classList.toggle('cell');
+            let divSize = `${Math.floor(gridWidth/gridSize)}px`;
+            cell.style.width = divSize;
+            cell.style.height = divSize;
+            console.log(`gridWidth: ${gridWidth} gridSize: ${gridSize} divSize: ${divSize}`);
             grid.appendChild(cell);
             if (i > 0 && j == 0) {
                 cell.style.clear = 'left';
             }
         }
     }
+    const allCells = document.querySelectorAll('.cell');
+
+    allCells.forEach((active) => {
+        active.addEventListener('mouseover', () => {
+        active.classList.add('active');
+        });
+    });
 }
 
-const allCells = document.querySelectorAll('.cell')
-
-allCells.forEach((active) => {
-    active.addEventListener('mouseover', () => {
-        active.classList.add('active');
-    });
-});
